a25d9db444e8451b60974c39be0002d82b4ce28d
dfrimpillo <rimpillodesserie@gmail.com>
2023-12-10 22:56:22 +0800

    reset pw

--- resetpw.php
+++ resetpw.php
@@ -10,14 +10,11 @@ if (isset($_POST["reset"])) {
     $newPassword = $_POST["password"];
     $confirmPassword = $_POST["repassword"];
 
-    // Validate input (you may add more validation)
     if ($newPassword !== $confirmPassword) {
         $errormsg = "Passwords do not match.";
     } else {
-        // Hash the new password (you should use a more secure hashing algorithm)
         $hashedPassword = password_hash($newPassword, PASSWORD_DEFAULT);
 
-        // Update the password in the database
         $stmt = $conn->prepare("UPDATE userinfo SET password = ? WHERE username = ?");
         $stmt->bind_param('ss', $hashedPassword, $username);
 
@@ -27,7 +24,6 @@ if (isset($_POST["reset"])) {
            echo "Error updating password. Please try again.";
         }
 
-        // Close the statement
         $stmt->close();
     }
 }
